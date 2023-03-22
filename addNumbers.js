b7ba9b2c4cfc530dff07639734eb3be67f911c9d
Christian <christianday954@ymail.com>
2023-03-22 15:29:22 -0700

    finish addNumbers

--- addNumbers.js
+++ addNumbers.js
@@ -4,3 +4,18 @@ const reader = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
+function addNumbers(sum, numsLeft, completionCallback) {
+  if (numsLeft > 0) {
+    reader.question("input number:", function(num) { 
+      const userNum = parseInt(num);
+      console.log(sum += userNum);
+      addNumbers(sum, numsLeft - 1, completionCallback)
+    })
+  } else {
+    reader.close();
+    return completionCallback(sum);
+  }
+}
+
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
